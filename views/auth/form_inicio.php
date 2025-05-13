487dc1f6682a931835759ada2611bdb66ccbd62e
Sebastian Felipe Araque Perez <sebaspipe18@gmail.com>
2025-05-12 19:29:10 -0500

    probando el estilo del index desde /proyecto/

--- views/auth/form_inicio.php
+++ views/auth/form_inicio.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="/../public/css/logIn.css">
+    <link rel="stylesheet" href="/proyecto/public/css/logIn.css">
 </head>
 <body>
 <?php if (isset($_GET['expirada']) && $_GET['expirada'] == 1): ?>
@@ -14,7 +14,7 @@
     <div class="container">
         <div class="login-box">
             <div class="logo">
-                <img src="/../public/img/ViccControlImg.png" alt="Logo VICControl">
+                <img src="/proyecto/public/img/ViccControlImg.png" alt="Logo VICControl">
             </div>
             <form action="/../controller/auth/logIn.php" method="POST">
                 <div class="input-group">
